2f44d19c7b557a25e76e3a47407b674f0f00cf77
cyby-001 <cbccc656@gmail.com>
2025-11-23 22:38:43 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,7 +2,16 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
+    },
+    bf: {
+        api: 'http://by.bfzyapi.com/api.php',
+        name: '暴风影音',
+    },
+    js: {
+        api: 'https://jszyapi.com/api.php/provide/vod',
+        name: '极速资源',
     }
+    
 };
 
 // 调用全局方法合并
